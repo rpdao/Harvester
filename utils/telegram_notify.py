bf8ce0441c364b32f02829c24cd026e9a7e3899f
RPDNA <113478245+rpdao@users.noreply.github.com>
2026-01-20 04:30:35 +0200

    Create telegram_notify.py

---
+++ utils/telegram_notify.py
@@ -0,0 +1,14 @@
+import logging
+
+import requests
+
+from config.settings import TOKEN, CHAT_ID
+from utils.dc_helpers import escape_markdown
+
+ADMIN_USERNAMES = ["@MarsohodRpDAO", "@prime7\_7\_7"]
+
+# === ОТПРАВКА ОПОВЕЩЕНИЯ О ТИКЕТЕ ===
+
+...
+
+__all__ = ["send_ticket_alert"]
