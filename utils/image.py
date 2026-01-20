71204508a05e887c9be560f26345592eace62438
RPDNA <113478245+rpdao@users.noreply.github.com>
2026-01-20 04:23:57 +0200

    Create image.py

---
+++ utils/image.py
@@ -0,0 +1,20 @@
+import os
+import logging
+import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from config.settings import BACKGROUND_PATH, FONT_PATH, BTC_IMAGE_OUTPUT
+
+# === ГЕНЕРАЦИЯ ИЗОБРАЖЕНИЯ С ЦЕНОЙ BTC ===
+
+...
+
+# === ГЕНЕРАЦИЯ ИЗОБРАЖЕНИй ДЛЯ КОМАНД GM И GN ===
+
+...
+
+__all__ = [
+    "create_price_image",
+    "create_greeting_image"
+]
